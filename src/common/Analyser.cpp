05c6d8b41a21bea4937d4209183503b63489aa8e
adikso <adam@zambrzycki.net>
2018-09-28 22:05:29 +0200

    Fix filtering compiler generated methods

--- src/common/Analyser.cpp
+++ src/common/Analyser.cpp
@@ -15,14 +15,22 @@ bool Analyser::process(Type *type) {
         return false;
     }
 
-    for (auto &method : type->allMethods) {
-        if (isCompilerGenerated(method)) {
+    auto it = std::begin(type->allMethods);
+
+    while (it != std::end(type->allMethods)) {
+        Method * method = *it.base();
+
+        bool compilerGenerated = isCompilerGenerated(method);
+        if (compilerGenerated) {
             method->isCompilerGenerated = true;
             if (config.noCompilerGenerated) {
-                type->allMethods.erase(std::remove(type->allMethods.begin(), type->allMethods.end(), method), type->allMethods.end());
+                it = type->allMethods.erase(it);
             }
         }
 
+        if (!compilerGenerated || !config.noCompilerGenerated) {
+            ++it;
+        }
     }
 
     return true;
@@ -42,7 +50,7 @@ bool Analyser::isCompilerGenerated(Method *method) {
             "__vec_ctor", "__vec_dtor", "__vec_ctor_vb", "__ehvec_ctor",
             "__ehvec_dtor", "__ehvec_ctor_vb", "__copy_ctor_closure", "__local_vftable_ctor_closure",
             "__placement_delete_closure", "__placement_arrayDelete_closure", "_man_vec_ctor",
-            "__man_vec_dtor", "__ehvec_copy_ctor", "__ehvec_copy_ctor_vb"
+            "__man_vec_dtor", "__ehvec_copy_ctor", "__ehvec_copy_ctor_vb", "`scalar deleting destructor'"
     };
 
     return method->isCompilerGenerated || method->name.rfind("std::", 0) == 0 || std::count(msvc.begin(), msvc.end(), method->name);
